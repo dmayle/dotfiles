6817c7e60a7afc18cdad73c79ffd69b6908f7f67
Douglas Mayle <douglas@mayle.org>
2021-06-21 09:21:46 +0200

    Make desktop home-manager config work

---
+++ home-manager/home-desktop.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./nixos.nix
+    ./home.nix
+  ];
+}
+
+
--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -1,12 +1,16 @@
 { config, pkgs, ... }:
 let
   unstableHomeManager = fetchTarball https://github.com/nix-community/home-manager/tarball/07f6c6481e0cbbcaf3447f43e964baf99465c8e1;
+  unstable = builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/0b876eaed3ed4715ac566c59eb00004eca3114e8;
+  unstablePkgs = import unstable { config.allowUnfree = true; };
 in
 {
   imports =
     [
       "${unstableHomeManager}/modules/services/wlsunset.nix"
     ];
+
+  programs.neovim.withPython = false;
 
   services.wlsunset = {
     package = unstablePkgs.wlsunset;
